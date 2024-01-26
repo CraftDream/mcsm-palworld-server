5d7b3626f9bab795c6e83207dc749b93cdc82c8f
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2024-01-26 16:42:47 +0800

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,5 +4,5 @@ DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 FILE_PATH="/workspace/backups/palworld-save-${DATE}.tar.gz"
 cd /workspace/Pal/ || exit
 
-tar -zcf "$FILE_PATH" "workspace/Saved/"
+tar -zcf "$FILE_PATH" "Saved/"
 echo "backup created at $FILE_PATH"
