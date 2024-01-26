f2dbd6ada488ac9afee2a5bdc508869bcf12e5a7
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2024-01-27 00:18:43 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ ! -f /workspace/PalServer.sh ]; then
     cp -r /workspace_mirror/* /workspace/
@@ -82,4 +82,5 @@ EOL
 printf "\e[0;32m*****正在启动服务器*****\e[0m\n"
 echo "${STARTCOMMAND}"
 su steam -c nohup "${STARTCOMMAND}" &
+
 rcon-cli
