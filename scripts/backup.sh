e97a4ffbcda4bbdd12ddbc7d98defcafb2d21e24
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2024-01-27 00:10:09 +0800

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ "${RCON_ENABLED}" = true ]; then
-        rcon-cli save
-fi
-
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 FILE_PATH="/workspace/backups/palworld-save-${DATE}.tar.gz"
 cd /workspace/Pal/ || exit
