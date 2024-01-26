372384374212e86f92d3341f698f017e9b5e2c28
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2024-01-26 13:32:32 +0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-if [ ! -f /workspace/PalServer.sh ]; then
-
-    if [[ ! "${PUID}" -eq 0 ]] && [[ ! "${PGID}" -eq 0 ]]; then
-        printf "\e[0;32m*****EXECUTING USERMOD*****\e[0m\n"
-        usermod -o -u "${PUID}" steam
-        groupmod -o -g "${PGID}" steam
-    else
-        printf "\033[31mRunning as root is not supported, please fix your PUID and PGID!\n"
-        exit 1
-    fi
-
-    mkdir -p /workspace/backups
-    chown -R steam:steam /workspace
-
-    if [ "${UPDATE_ON_BOOT}" = true ]; then
-        printf "\e[0;32m*****STARTING INSTALL/UPDATE*****\e[0m\n"
-        su steam -c '/home/steam/steamcmd/steamcmd.sh +force_install_dir "/workspace" +login anonymous +app_update 2394010 validate +quit'
-    fi
+if [[ ! "${PUID}" -eq 0 ]] && [[ ! "${PGID}" -eq 0 ]]; then
+    printf "\e[0;32m*****EXECUTING USERMOD*****\e[0m\n"
+    usermod -o -u "${PUID}" steam
+    groupmod -o -g "${PGID}" steam
+else
+    printf "\033[31mRunning as root is not supported, please fix your PUID and PGID!\n"
+    exit 1
 fi
+
+mkdir -p /palworld/backups
+chown -R steam:steam /palworld
+
+if [ "${UPDATE_ON_BOOT}" = true ]; then
+    printf "\e[0;32m*****STARTING INSTALL/UPDATE*****\e[0m\n"
+    su steam -c '/home/steam/steamcmd/steamcmd.sh +force_install_dir "/palworld" +login anonymous +app_update 2394010 validate +quit'
+fi
+
+term_handler() {
+    if [ "${RCON_ENABLED}" = true ]; then
+        rcon-cli save
+        rcon-cli shutdown 1
+    else # Does not save
+        kill -SIGTERM "$(pidof PalServer-Linux-Test)"
+    fi
+    tail --pid=$killpid -f 2>/dev/null
+}
+
+trap 'term_handler' SIGTERM
+
+./start.sh &
+killpid="$!"
+wait $killpid
