60fc47cdd67860d6b2193d0c578a296e93ee67b0
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2024-01-27 13:04:35 +0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-if [[ ! "${PUID}" -eq 0 ]] && [[ ! "${PGID}" -eq 0 ]]; then
-    printf "\e[0;32m*****执行用户组命令*****\e[0m\n"
-    usermod -o -u "${PUID}" steam
-    groupmod -o -g "${PGID}" steam
-else
-    printf "\033[31m不支持以root运行, 请修复你的 PUID 和 PGID!\n"
-    exit 1
+if [ ! -f /workspace/PalServer.sh ]; then
+    cp -r /workspace_mirror/* /workspace/
+    chown -R steam:steam /workspace
 fi
 
-mkdir -p /workspace/backups
-chown -R steam:steam /workspace
+term_handler() {
+    if [ "${RCON_ENABLED}" = true ]; then
+        rcon-cli save
+        rcon-cli shutdown 1
+    else # Does not save
+        kill -SIGTERM "$(pidof PalServer-Linux-Test)"
+    fi
+    tail --pid=$killpid -f 2>/dev/null
+}
 
-if [ "${UPDATE_ON_BOOT}" = true ]; then
-    printf "\e[0;32m*****开始安装/更新*****\e[0m\n"
-    su steam -c '/home/steam/steamcmd/steamcmd.sh +force_install_dir "/workspace" +login anonymous +app_update 2394010 validate +quit'
-fi
+trap 'term_handler' SIGTERM
+
+palstart &
+killpid="$!"
+wait $killpid
