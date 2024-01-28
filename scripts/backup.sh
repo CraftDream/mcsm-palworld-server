0ef69493794c8599d3640b62591d872acc79f30b
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2024-01-28 12:44:54 +0800

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-FILE_PATH="./backups/palworld-save-${DATE}.tar.gz"
-tar -zcf "$FILE_PATH" "./Pal/Saved/"
+FILE_PATH="/workspace/backups/palworld-save-${DATE}.tar.gz"
+tar -zcf "$FILE_PATH" "/workspace/Pal/Saved/"
 printf "备份点已创建至: $FILE_PATH"
